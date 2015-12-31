b86c402ada0bf793c71c89c186471b9d5c1d7f51
Pulla Rao <vpullarao@hotmail.com>
2015-12-31 18:32:26 +0530

    added winston logging framwork

--- utils/database.js
+++ utils/database.js
@@ -4,8 +4,9 @@ var mongoUrl = 'mongodb://127.0.0.1:27017/sample';
 var db;
 exports.connect = function(callback) {
 	MongoClient.connect(mongoUrl, function(err, database) {
-		if (err)
+		if (err){						
 			throw err;
+		}				
 		db = database;		
 		callback();
 	});
---
+++ utils/logger.js
@@ -0,0 +1,36 @@
+var winston = require('winston');
+winston.emitErrs = true;
+
+var logger = new winston.Logger({
+	transports : [ new winston.transports.File({
+		name : 'error-file',
+		level : 'error',
+		filename : '../logs/filelog-error.log',
+		json : true,
+		maxsize : 5242880, // 5MB
+		maxFiles : 5
+	}), new winston.transports.File({
+		name : 'info-file',
+		level : 'info',
+		filename : '../logs/filelog-info.log',
+		json : true,
+		maxsize : 5242880, // 5MB
+		maxFiles : 5,
+		colorize : false
+	}), new winston.transports.Console({
+		level : 'debug',
+		json : false,
+		colorize : false
+	}) ],
+	exceptionHandlers : [ new winston.transports.File({
+		filename : '../logs/filelog-exceptions.log'
+	}) ],
+	exitOnError : false
+});
+
+module.exports = logger;
+module.exports.stream = {
+	write : function(message, encoding) {
+		logger.info(message);
+	}
+};
